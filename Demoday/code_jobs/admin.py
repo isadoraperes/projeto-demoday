9a4e1fa7bfe2bb1151cba981f230ba82f162419e
Isadora <isadoraoperes@gmail.com>
2019-08-17 23:30:39 -0300

    subir repositório organizado

--- Demoday/code_jobs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-
-admin.site.register(models.Pessoa)
-admin.site.register(models.Categoria)
-admin.site.register(models.Curso)
--- Demoday/code_jobs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CodeJobsConfig(AppConfig):
-    name = 'code_jobs'
